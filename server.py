18692604a14a985e3c8d91ecc13b9a58d5546e99
daniel <danielrc2000@gmail.com>
2025-03-28 15:14:56 +0100

    Comentarios para mejor entendimiendo del codigo

--- server.py
+++ server.py
@@ -2,18 +2,25 @@ from flask import Flask, jsonify
 from flask_talisman import Talisman
 import os
 
+# Crear la aplicación Flask
 app = Flask(__name__)
+
+# Forzar HTTPS para todas las solicitudes
 Talisman(app)  # Fuerza HTTPS
 
 # Cargar la clave AES desde una variable de entorno
 AES_KEY = os.getenv("AES_KEY")
 
+# Verificar que la clave AES esté presente en las variables de entorno
 if not AES_KEY:
     raise ValueError("Falta la clave AES en las variables de entorno")
 
+# Crear un endpoint para obtener la clave AES
 @app.route("/get-key", methods=["GET"])
 def get_key():
+    # Devuelve la clave AES en formato JSON
     return jsonify({"aes_key": AES_KEY}), 200
 
+# Iniciar la aplicación Flask
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
